0f0c245a3e9c2c7e56646ecff1aec9654cb8fa70
Artemka-py <djartlyt@gmail.com>
2021-09-30 16:24:57 +0300

    Add styled components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components";
 
-const App = () => <h1>Hello world!!5656! {new Date().toLocaleDateString()}</h1>;
+const Header = styled.h1`
+  font-size: 50px;
+`;
+
+const App = () => (
+  <Header>Hello world!!5656! {new Date().toLocaleDateString()}</Header>
+);
 
 ReactDOM.render(
   <React.StrictMode>
